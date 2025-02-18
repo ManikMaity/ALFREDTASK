d298f2403b680d7a78a938b8afe5eb53ae39feb2
Manik Maity <manikmaity156@gmail.com>
2025-02-18 11:07:56 +0530

    Added useSignin hook and shadcn toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 import AppRoute from "./pages/AppRoute";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppRoute />
+      <Toaster />
     </QueryClientProvider>
   );
 }
--- frontend/src/hooks/auth/useSignup.jsx
+++ frontend/src/hooks/auth/useSignup.jsx
@@ -1,6 +1,37 @@
+import { useMutation } from "@tanstack/react-query";
+import { signupRequest } from "@/apis/user";
+import { useToast } from "../use-toast";
 
 function useSignup() {
-  
+
+    const { toast } = useToast();
+
+    
+    const { mutateAsync : signupMutateAsync, isLoading, error, data, isSuccess } = useMutation({
+        mutationFn: signupRequest,
+        onSuccess: () => {
+            toast({
+                title: "Successfully signed up",
+                description: "You will be redirected to signin page in a few seconds",
+                type: "success",
+            });
+        },
+        onError: (error) => {
+            toast({
+                title: "Error signing up",
+                type: "error",
+            });
+        },
+    });
+
+
+    return {
+        signupMutateAsync,
+        isLoading,
+        error,
+        data,
+        isSuccess
+    };
 }
 
-export default useSignup
+export default useSignup;
