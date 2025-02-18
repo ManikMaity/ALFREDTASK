151964e44b1a48166cef747cb62fc2fb2b14e584
Manik Maity <manikmaity156@gmail.com>
2025-02-18 10:59:27 +0530

    Added getUserByEmail repo func

--- backend/src/repositories/user.repo.js
+++ backend/src/repositories/user.repo.js
@@ -4,6 +4,10 @@ import crudFunctions from "./crud.js";
 
 const userRepo = {
     ...crudFunctions(userModel),
+    getUserByEmail : async function (email) {
+        const user = await userModel.findOne({email : email});
+        return user
+    }
 }
 
 export default userRepo;
