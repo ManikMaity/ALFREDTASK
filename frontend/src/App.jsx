1f77f1d99d86da3bec7c83ddb04a8f299ce69d9e
Manik Maity <manikmaity156@gmail.com>
2025-02-18 09:42:52 +0530

    Initial frontend with shadcn done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <main>
+      <Button>Click</Button>
+    </main>
+  )
+}
+
+export default App
